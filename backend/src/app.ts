13c7cbc4a1349b41214dbe2522b3e50528ec5c0e
ocitocit <ocritocrit@gmail.com>
2023-04-08 00:56:13 +0700

    adding code for handling error,

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,16 @@ import NoteModel from "./models/note";
 const app = express();
 
 app.get("/", async (req, res) => {
-  const note = await NoteModel.find().exec();
-  res.status(200).json(note);
+  try {
+    throw Error("terjadi error");
+    const note = await NoteModel.find().exec();
+    res.status(200).json(note);
+  } catch (error) {
+    console.error(error);
+    let errorMessage = "An unknown error occured";
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({ error: errorMessage });
+  }
 });
 
 export default app;

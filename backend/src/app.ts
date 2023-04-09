a35d3d2fac9594cd20967d76de6870b7cd732df1
ocitocit <ocritocrit@gmail.com>
2023-04-09 22:29:45 +0700

    error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,29 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("/", async (req, res, next) => {
   try {
-    throw Error("terjadi error");
+    // throw Error("terjadi error");
     const note = await NoteModel.find().exec();
     res.status(200).json(note);
   } catch (error) {
-    console.error(error);
-    let errorMessage = "An unknown error occured";
-    if (error instanceof Error) errorMessage = error.message;
-    res.status(500).json({ error: errorMessage });
+    next(error);
   }
+});
+
+app.use((res, req, next) => {
+  next(Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occured";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;

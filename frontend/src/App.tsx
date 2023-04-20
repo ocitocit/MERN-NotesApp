1b5cebe0a482030aeb871bc899bee60ea06081e6
ocitocit <ocritocrit@gmail.com>
2023-04-20 23:44:57 +0700

    update styling Notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import Note from "./components/Note";
 import { Note as NoteModel } from "./models/note";
+import styles from "./styles/NotesPage.module.css";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -22,11 +24,15 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {notes.map((note) => (
-        <Note note={note} key={note._id} />
-      ))}
-    </div>
+    <Container>
+      <Row xs={1} md={2} xl={3} className="g-4">
+        {notes.map((note) => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,19 +1,30 @@
 import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
+import styles from "../styles/Note.module.css";
+import { formatDate } from "../utils/formatDate";
 
 interface NoteProps {
   note: NoteModel;
+  className?: string;
 }
 
-const Note = ({ note }: NoteProps) => {
+const Note = ({ note, className }: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
 
+  let createUpdatedDate: string;
+  if (updatedAt > createdAt) {
+    createUpdatedDate = `Updated: ${formatDate(updatedAt)}`;
+  } else {
+    createUpdatedDate = `Created: ${formatDate(createdAt)}`;
+  }
+
   return (
-    <Card>
-      <Card.Body>
+    <Card className={`${styles.noteCard} ${className}`}>
+      <Card.Body className={styles.cardBody}>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>{text}</Card.Text>
+        <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
+      <Card.Footer className="text-muted">{createUpdatedDate}</Card.Footer>
     </Card>
   );
 };

60b312b5cf59c372fb7974a162fbee0b311011f7
ocitocit <ocritocrit@gmail.com>
2023-05-08 11:52:26 +0700

    logout and authenticated user

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -3,6 +3,23 @@ import createHttpError from "http-errors";
 import UserModel from "../models/user";
 import bcrypt from "bcrypt";
 
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+  const authenticatedUserId = req.session.userId;
+
+  try {
+    if (!authenticatedUserId) {
+      throw createHttpError(401, "User not authenticated");
+    }
+
+    const user = await UserModel.findById(authenticatedUserId)
+      .select("+email")
+      .exec();
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
 interface SignUpBody {
   username?: string;
   email?: string;
@@ -98,4 +115,14 @@ export const login: RequestHandler<
   } catch (error) {
     next(error);
   }
+};
+
+export const logout: RequestHandler = (req, res, next) => {
+  req.session.destroy((error) => {
+    if (error) {
+      next(error);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 };
